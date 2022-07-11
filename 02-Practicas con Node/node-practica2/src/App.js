f81a9dee15d931404f3c1307e706f04cf2524b66
ApasoftTraining <apasoft.training@gmail.com>
2022-07-11 14:44:35 +0200

    m1

---
+++ 02-Practicas con Node/node-practica2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Coche from './Componentes/Coche';
+
+function App() {
+  return (
+    <div className="App">
+      <Coche />
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-Practicas con Node/node-practica2/src/Componentes/Coche.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Coche() {
+  return (
+    <div>
+        <h2>Coche</h2>
+        <h3>Esto es un coche</h3>
+    </div>
+  )
+}
+
+export default Coche
