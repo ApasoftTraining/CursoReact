258eb68109f088061aec8ccdff4430a8ae0ca53e
ApasoftTraining <apasoft.training@gmail.com>
2022-07-11 10:27:30 +0200

    m1

---
+++ 02-Practicas con Node/node-inicial/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>aplicacion REACT</h2>
+    </div>
+  );
+}
+
+export default App;
